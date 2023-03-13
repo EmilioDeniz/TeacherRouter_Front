071cd1a5152c6a4c7407c9a854383b82728a4de2
emmartel <emilio_martel@outlook.es>
2023-03-13 14:10:04 +0000

    creating project structure

---
+++ README.md
@@ -0,0 +1,7 @@
+# TeacherRouter
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.2.
+
+## Development server
+
+nuestro proyecto de ps
---
+++ src/app/teacher-router/teacher-router.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TeacherRouterRoutingModule } from './teacher-router-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TeacherRouterRoutingModule
+  ]
+})
+export class TeacherRouterModule { }
