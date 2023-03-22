37d40ea21fd26be1027a397a89cbf8d21b8ce8d7
emmartel <emilio_martel@outlook.es>
2023-03-22 11:03:44 +0000

    routes and LoginPage created

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InfoPageComponent } from './pages/info-page/info-page.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'login', component: LoginComponent
+      },
+      {
+        path: '**', redirectTo: 'login'
+      }
+    ]
+  }
+  
   
 ];
 
