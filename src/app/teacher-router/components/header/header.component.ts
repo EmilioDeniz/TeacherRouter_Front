6e3305dea509622796c56ee4b2ba93f4f0ccdb02
AlejandroOrtegaG <alejandroog99@gmail.com>
2023-03-26 14:06:43 +0100

    Header fixed

--- src/app/teacher-router/components/header/header.component.ts
+++ src/app/teacher-router/components/header/header.component.ts
@@ -10,7 +10,16 @@ export class HeaderComponent {
   constructor(private sidenavService: SidenavService) {
   }
 
+  header_noLogged: boolean = true;
+  header_Logged: boolean = false;
+
   toggleSidenav() {
     this.sidenavService.toggle();
+  }
+
+  switch(){
+    this.header_Logged=!this.header_Logged;
+    this.header_noLogged=!this.header_noLogged;
+
   }
 }
