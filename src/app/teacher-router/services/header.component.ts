3846a944e43d1028aae5e7cdb138d9edc39650d9
EmilioDeniz <113598358+EmilioDeniz@users.noreply.github.com>
2023-04-20 10:23:27 +0100

    Aparición del sidenav cambiada

---
+++ src/app/teacher-router/services/header.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { SidenavService } from "../../services/sidenav.service";
+import { Router } from '@angular/router';
+import { VisitorSidenavComponent } from '../visitor-sidenav/visitor-sidenav.component';
+import { ComponentFactoryResolver, Injector, ApplicationRef,EmbeddedViewRef } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  constructor(private sidenavService: SidenavService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver,
+    private injector: Injector,
+    private appRef: ApplicationRef) {
+  }
+
+  header_noLogged: boolean = true;
+  header_Logged: boolean = false;
+
+  toggleSidenav() {
+    this.sidenavService.toggle();
+  }
+
+  switch() {
+    this.header_Logged = !this.header_Logged;
+    this.header_noLogged = !this.header_noLogged;
+  }
+
+  toggleVisitorSidenav() {
+    const factory = this.componentFactoryResolver.resolveComponentFactory(VisitorSidenavComponent);
+    const componentRef = factory.create(this.injector);
+    this.appRef.attachView(componentRef.hostView);
+    const domElem = (componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement;
+    document.body.appendChild(domElem);
+  }
+
+  isAdmin() {
+    return false;
+  }
+}
