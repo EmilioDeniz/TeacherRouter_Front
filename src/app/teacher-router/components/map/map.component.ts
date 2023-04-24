babc94113cd61eeefb392c6d0e7dc44edc339ad4
EmilioDeniz <welidrams112@gmail.com>
2023-04-24 13:33:28 +0100

    Mapa de Google añadido (pendiente Token)

--- src/app/teacher-router/components/map/map.component.ts
+++ src/app/teacher-router/components/map/map.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+export class MapComponent implements OnInit {
+  
+  map: google.maps.Map;
 
-export class MapComponent {
+  constructor() { }
+
+  ngOnInit() {
+    this.initMap();
+  }
+
+  initMap() {
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 28.126033, lng: -15.454997 },
+      zoom: 8
+    });
+  }
 
 }
+
