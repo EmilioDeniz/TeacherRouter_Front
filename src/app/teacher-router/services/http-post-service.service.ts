87fcffe50ae7e6ae1356afe9485db6804fd936e7
emmartel <emilio_martel@outlook.es>
2023-05-10 11:30:15 +0100

    update ip and peticionServer

--- src/app/teacher-router/services/http-post-service.service.ts
+++ src/app/teacher-router/services/http-post-service.service.ts
@@ -1,19 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { catchError, map, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpPostServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get apiUrl() {
+    return `${environment.apiUrl}`;
+  }
 
   peticionSever(url: string, data: FormData): any{
 
-    console.log("url: ", url);
-    console.log("data: ", data);
-    
+    return this.http.post<any>(`${this.apiUrl}/${url}`, data);   
 
   }
+
+  
   
 
 
